08e71b2a8fe9e58ed1f4138a22a9dcb6051aed64
Matheus Leal <matheusaquino@id.uff.br>
2020-04-27 21:43:40 -0300

    remove  unecessary code

--- src/store/wallet/actions.js
+++ src/store/wallet/actions.js
@@ -1,13 +1,11 @@
 export function decrement({ commit, state }, payload) {
   const totalValue = Number(state.value) - Number(payload)
-  console.log('d', totalValue)
   if(totalValue >= 0) commit('setValue', totalValue)
   if(totalValue < 0)  commit('setValue', 0)
 }
 
 export function increment({ commit, state }, payload) {
   const totalValue = Number(state.value) + Number(payload)
-  console.log('i', totalValue)
   if (totalValue <= state.totalAmount && totalValue > 0) commit('setValue', totalValue)
   if (totalValue >= state.totalAmount) commit('setValue', state.totalAmount)
 }
